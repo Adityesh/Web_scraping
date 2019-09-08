8e0a3f6d877cd304baeedbb57cda3c6c4060da67
Adityesh <adityeshmishra98@gmail.com>
2019-09-08 23:57:41 +0530

    Scraped projects and added to csv file

--- scrape.py
+++ scrape.py
@@ -8,9 +8,17 @@ response = requests.get('https://adityesh.github.io/Adityesh/')
 soup = BeautifulSoup(response.text,'html.parser')
 projects = soup.findAll(class_= "project-container")
 
-for project in projects:
-    title = project.findAll(class_="right-container")
-    for project_title in title:
-        print(">>>" + project_title.find('span').get_text())
-        print("----------------")
+with open('project.csv','w') as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ['Title','Project-Name']
+    csv_writer.writerow(headers)
+
+
+    for project in projects:
+        title = project.findAll(class_="right-container")
+        column_1 = "Project Title"
+        for project_title in title:
+            project_heading = project_title.find('span').get_text()
+            print("Project Title : " +project_heading + "\n")
+            csv_writer.writerow([column_1,project_heading])
     
