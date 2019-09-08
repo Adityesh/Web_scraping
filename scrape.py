1341af7f3865da69662f4585d62277a27b92472b
Adityesh <adityeshmishra98@gmail.com>
2019-09-08 23:35:25 +0530

    scrape projects from my personal website

--- scrape.py
+++ scrape.py
@@ -6,5 +6,11 @@ from csv import writer
 response = requests.get('https://adityesh.github.io/Adityesh/')
 
 soup = BeautifulSoup(response.text,'html.parser')
+projects = soup.findAll(class_= "project-container")
 
-print(soup)
+for project in projects:
+    title = project.findAll(class_="right-container")
+    for project_title in title:
+        print(">>>" + project_title.find('span').get_text())
+        print("----------------")
+    
